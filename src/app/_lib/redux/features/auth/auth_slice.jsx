ed4a4d9c2a76a40fe4649860e81ff10dbd393641
sachintha <sachintha0603@gmail.com>
2024-08-13 16:50:56 +0530

    #6 Fix: .

--- src/app/_lib/redux/features/auth/auth_slice.jsx
+++ src/app/_lib/redux/features/auth/auth_slice.jsx
@@ -1,19 +1,22 @@
-import dynamic from "next/dynamic";
 import { createSlice } from "@reduxjs/toolkit";
 
-// Ensure that `localStorage` is only accessed in the browser
+const safeParseJSON = (item) => {
+  try {
+    return JSON.parse(item);
+  } catch (error) {
+    console.error("Error parsing JSON", error);
+    return null;
+  }
+};
+
 const initialState = {
   user: null,
   accessToken: null,
-  refreshToken: null,
 };
 
 if (typeof window !== "undefined") {
-  console.log("Initializing state from localStorage");
-  initialState.user = JSON.parse(localStorage.getItem("user")) || null;
+  initialState.user = safeParseJSON(localStorage.getItem("user")) || null;
   initialState.accessToken = localStorage.getItem("accessToken") || null;
-  initialState.refreshToken = localStorage.getItem("refreshToken") || null;
-  console.log("Initial state:", initialState);
 }
 
 const authSlice = createSlice({
@@ -22,32 +25,18 @@ const authSlice = createSlice({
   reducers: {
     userLoggedIn: (state, action) => {
       state.accessToken = action.payload.accessToken;
-      state.refreshToken = action.payload.refreshToken;
       state.user = action.payload.user;
 
       if (typeof window !== "undefined") {
         localStorage.setItem("accessToken", action.payload.accessToken);
-        localStorage.setItem("refreshToken", action.payload.refreshToken);
         localStorage.setItem("user", JSON.stringify(action.payload.user));
-      }
-    },
-    userLoggedOut: (state) => {
-      state.accessToken = null;
-      state.refreshToken = null;
-      state.user = null;
-
-      if (typeof window !== "undefined") {
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
-        localStorage.removeItem("user");
       }
     },
   },
 });
 
-export const { userLoggedIn, userLoggedOut } = authSlice.actions;
+export const { userLoggedIn } = authSlice.actions;
 export default authSlice.reducer;
 
 export const selectCurrentUser = (state) => state.auth.user;
 export const selectCurrentAccessToken = (state) => state.auth.accessToken;
-export const selectCurrentRefreshToken = (state) => state.auth.refreshToken;
